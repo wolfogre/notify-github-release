cf252077080d533b625eb8b526c1de04170543c3
Jason Song <i@wolfogre.com>
2018-07-23 14:45:59 +0800

    Revert "Check tag only when repo has no release"
    This reverts commit 625cfbbb586c1c1693b670091b3985530b53e1aa.

--- notifier.py
+++ notifier.py
@@ -61,13 +61,13 @@ class Notifier:
         for repo in repos:
             while True:
                 try:
-                    release, check_tag = self.__get_latest_release_within_one_day(repo)
+                    release = self.__get_latest_release_within_one_day(repo)
                     if release is not None:
                         result[repo.id] = {
                             "repo": repo,
                             "release": release,
                         }
-                    elif check_tag:
+                    else:
                         tag = self.__get_latest_tag_within_one_day(repo)
                         if tag is not None:
                             result[repo.id] = {
@@ -96,7 +96,7 @@ class Notifier:
             result[repo.id] = repo
         return result
 
-    def __get_latest_release_within_one_day(self, repo: Repository) -> (GitRelease, bool):
+    def __get_latest_release_within_one_day(self, repo: Repository) -> GitRelease:
         # Draft releases and prereleases are not returned repo.get_latest_release(), so use get_releases()
         self.logger.info("start get latest release of %s", repo.full_name)
         release = None
@@ -108,7 +108,7 @@ class Notifier:
 
         if release is None:
             self.logger.info("%s has no release", repo.full_name)
-            return None, True
+            return None
 
         published_at = pytz.utc.localize(release.published_at).astimezone(self.__local_timezone)
         self.logger.info("%s's latest release: %s %s %s, %s ago",
@@ -116,8 +116,8 @@ class Notifier:
                          release.id, release.title, published_at.isoformat(),
                          (self.__start_time - published_at))
         if (self.__start_time - published_at).total_seconds() <= 24 * 60 * 60:
-            return release, False
-        return None, False
+            return release
+        return None
 
     def __get_latest_tag_within_one_day(self, repo: Repository) -> dict:
         # visiting the html page is the only way to get the latest tag
